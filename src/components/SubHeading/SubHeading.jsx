aa65aa91e15e10c2caae0e55edb0f8186783f31e
CrashedOut93 <crashstocks@gmail.com>
2022-03-26 21:53:47 -0500

    Subheading
    Working on subheading

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { images } from '../../constants';
 
 const SubHeading = () => (
-  <div>
-    SubHeading
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">Title</p>
   </div>
 );
 
