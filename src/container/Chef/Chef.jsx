89934326097f3a8f752de1ddedf8e7a8159ba76a
CrashedOut93 <crashstocks@gmail.com>
2022-04-27 02:43:43 -0500

    Update Chef.jsx

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -14,12 +14,14 @@ const Chef = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chef's Word" />
       <h1 className='headtext__cormorant'>What we believe in</h1>
-    </div>
+
 
     <div className='app__chef-content'>
       <div className='app__chef-content_quote'>
         <img src={images.quote} alt="quote" />
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
       </div>
+    </div>
     </div>
   </div>
 );
