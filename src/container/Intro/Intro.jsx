7be1ba081dd3a9f0133f21880fbe76e50bc4cb88
CrashedOut93 <crashstocks@gmail.com>
2022-05-10 23:35:17 -0500

    Update Intro.jsx

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -4,16 +4,19 @@ import { meal } from '../../constants';
 
 import './Intro.css';
 
-const Intro = () => (
+const Intro = () => {
+  
+  return (
   <div className="app__video">
     <video 
       src={meal}
+      ref={vidRef}
       type="video/mp4"
       loop
       controls={false}
       muted
     />
   </div>
-);
+  )};
 
 export default Intro;
